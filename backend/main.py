8ce2316288c26f17d05d95704876e9d000fd633a
Ben Mercer <104690692+benjamano@users.noreply.github.com>
2023-12-15 01:13:13 +0000

    Bug Fix

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../frontend/templates', static_folder='../frontend/static')
+
 
 @app.route("/")
 def hello_world():
     return render_template("index.html")
 
+
+if __name__ == '__main__':
+    app.run()

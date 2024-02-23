1f7e82a65a23bb283e142fd166d6e39ef634c019
Ben Mercer <104690692+benjamano@users.noreply.github.com>
2024-02-23 23:05:42 +0000

    Adding a login page

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__, template_folder='../frontend/templates', static_folder='../frontend/static')
 
@@ -10,6 +10,22 @@ def home():
 @app.route("/projects")
 def LaserTag():
     return render_template("projects.html")
+
+@app.route("/login", methods = ['GET', 'POST'])
+def login():
+    
+    if request.method == "POST":
+    
+        username = request.form["username"]
+        password = request.form["password"]
+        
+        if username == "ben" and password == "B3n1sCool":
+            return render_template("system/home.html")
+        
+        else:
+            flash("Invalid username or password")
+    
+    return render_template("login.html")
 
 if __name__ == '__main__':
     app.run()

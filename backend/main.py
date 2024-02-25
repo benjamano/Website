59aca82f6ac2376ef2b145c710cf5ede9765e2b5
Ben Mercer <104690692+benjamano@users.noreply.github.com>
2024-02-25 02:14:45 +0000

    c

--- backend/main.py
+++ backend/main.py
@@ -160,28 +160,21 @@ def newParty():
         
         max_parties = {1: 2, 3: 2, 5: 2, 2: 1, 4: 1, 6: 1}
         
-        isopen = {(1, "10:30"): False, (1, "11:30"): False, (1, "15:00"): False, (1, "16:00"): False,
-                    (2, "10:30"): False, (2, "11:30"): False, (2, "15:00"): False, (2, "16:00"): False,
-                    (3, "10:30"): False, (3, "11:30"): False, (3, "15:00"): False, (3, "16:00"): False,
-                    (4, "10:30"): False, (4, "11:30"): False, (4, "15:00"): False, (4, "16:00"): False,
-                    (5, "10:30"): False, (5, "11:30"): False, (5, "15:00"): False, (5, "16:00"): False,
-                    (6, "10:30"): False, (6, "11:30"): False, (6, "15:00"): False, (6, "16:00"): False}
+        isopen = {}
         
         times = ["10:30", "11:30", "15:00", "16:00"]
         
         for i in range(1, 7):
-            
+            isopen[i] = {}
             for time in times:
-                
                 q.execute(partyexists, [date, i, time])
-                
                 noofparties = q.fetchall()
-                
-                if noofparties[0][0] < max_parties[i]:
-                    
-                    isopen[(i, time)] = True
+                if noofparties[0][0] <= max_parties[i]:
+                    isopen[i][time] = True
+                else:
+                    isopen[i][time] = False
 
-        app.logger.info(f"\n\nIs open: {isopen}\n\n{isopen[(1, '10:30')]}\n\n{isopen[(5, '16:00')]}")
+        app.logger.info(f"\n\nIs open: {isopen}\n\n{isopen[1]['10:30']}\n\n{isopen[5]['16:00']}")
 
         return render_template("/system/newparty.html", isopen = isopen)
 

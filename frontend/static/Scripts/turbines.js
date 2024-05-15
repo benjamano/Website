bbb90198d52065e480615157a5743fbedc70b341
Ben Mercer <benmercer76@btinternet.com>
2024-05-15 18:46:23 +0100

    Adding multiple wind turbines which spin depending on how much energy is being generated that day

--- frontend/static/Scripts/turbines.js
+++ frontend/static/Scripts/turbines.js
@@ -1,3 +1,5 @@
+var rotationSpeed = 0.001;
+
 document.addEventListener('DOMContentLoaded', () => {
     let canvas = document.getElementById('turbineCanvas');
     let ctx = canvas.getContext('2d');
@@ -11,33 +13,30 @@ document.addEventListener('DOMContentLoaded', () => {
     window.addEventListener('resize', resizeCanvas);
     resizeCanvas();
 
-    function fetchLCTData() {
-        fetch('/getlctdata')
-            .then(response => {
-                if (response.ok) {
-                    return response.json();
-                } else {
-                    throw new Error('Failed to fetch fault data');
-                }
-            })
-            .then(data => {
-                console.log('Fetched lct data:', data);
-                return data;
-            })
-            .catch(error => {
-                console.error('Error fetching fault data:', error);
-            });
-    }
-
     function drawTurbineStem() {
-        let stemHeight = canvas.height * 0.4;
+        let stemHeight = canvas.height * 0.5;
         ctx.fillStyle = 'white';
         ctx.fillRect(canvas.width / 2 - 5, canvas.height - stemHeight, 10, stemHeight);
     }
 
+    function drawTurbineCircle() {
+        let centerX = canvas.width / 2;
+        let centerY = canvas.height - (canvas.height * 0.45);
+        let radius = 7;
+
+        ctx.beginPath();
+        ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+        ctx.fillStyle = 'black';
+        ctx.fill();
+        ctx.strokeStyle = 'white';
+        ctx.lineWidth = 2;
+        ctx.stroke();
+        ctx.closePath();
+    }
+
     function drawTurbineBlades(angle) {
         let centerX = canvas.width / 2;
-        let centerY = canvas.height - (canvas.height * 0.2);
+        let centerY = canvas.height - (canvas.height * 0.47);
         let bladeLength = 50;
 
         ctx.save();
@@ -52,30 +51,61 @@ document.addEventListener('DOMContentLoaded', () => {
             ctx.lineTo(bladeLength, -10);
             ctx.closePath();
             ctx.fill();
-            ctx.rotate((2 * Math.PI) / 3); // Rotate 120 degrees for each blade
+            ctx.rotate((2 * Math.PI) / 3);
         }
 
         ctx.restore();
     }
 
     let angle = 0;
-    let rotationSpeed = 0.001;
+    let numofTurbines = 3;
 
     function draw() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        drawTurbineStem();
-        drawTurbineBlades(angle);
-        angle += rotationSpeed;
-        requestAnimationFrame(draw);
-    }
+        let turbineOffset =  -(canvas.width * numofTurbines * 0.06); 
 
-    lctdata = fetchLCTData();
-
-    for (const record of lctdata) {
-        if (record.type == "windturbine") {
-            rotationSpeed += (record.ExportRating * 0.001);
+        for (let i = 0; i < numofTurbines; i++) {
+            ctx.save();
+            ctx.translate(turbineOffset * i, 0);
+            drawTurbineStem();
+            drawTurbineBlades(angle);
+            drawTurbineCircle();
+            angle += rotationSpeed;
+            ctx.restore();
         }
+        requestAnimationFrame(draw);
     }
 
     draw();
 });
+
+function fetchLCTData() {
+    return fetch('/getlctdata')
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error('Failed to fetch fault data');
+            }
+        })
+        .then(data => {
+            console.log('Fetched lct data:', data);
+            return data;
+        })
+        .catch(error => {
+            console.error('Error fetching fault data:', error);
+        });
+}
+
+fetchLCTData()
+    .then(lctdata => {
+
+        for (const record of lctdata) {
+            if (record.type == "Wind") {
+                rotationSpeed += record.exportrating_mw * 0.003;
+            }
+        }
+    })
+    .catch(error => {
+        console.error('Error fetching fault data:', error);
+    });

a6d4d11fa2da4652fac5b56d0411f7c025bc8295
Ben Mercer <benmercer76@btinternet.com>
2024-12-09 00:07:35 +0000

    Changed which class it looks for

--- frontend/static/Scripts/imagesliders.js
+++ frontend/static/Scripts/imagesliders.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-        const sliders = document.querySelectorAll(".imageRevealBox");
+        const sliders = document.querySelectorAll(".sliderReveal");
   
         sliders.forEach((slider) => {
           const afterImage = slider.querySelector(".slider-after");
